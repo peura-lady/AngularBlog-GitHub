d51ab15b442b97fd8cb367cbee86ce26f2720be4
peura-lady <anna.donninen94@gmail.com>
2022-01-06 12:37:19 +0200

    keeping the token

--- angular-blog/src/app/shared/interfaces.ts
+++ angular-blog/src/app/shared/interfaces.ts
@@ -1,8 +1,10 @@
 export interface User {
+    returnSecureToken?: boolean
     email: string
     password: string
 }
 
 export interface FbAuthResponse {
     idToken: string
+    expiresIn: string
 }
