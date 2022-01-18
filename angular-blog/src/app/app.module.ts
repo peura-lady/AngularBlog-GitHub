3b5d01b9a573d72bcfc01b9bca460b4de1ea5ffb
peura-lady <anna.donninen94@gmail.com>
2022-01-18 17:01:28 +0200

    added PWA

--- angular-blog/src/app/app.module.ts
+++ angular-blog/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule, Provider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { QuillModule } from 'ngx-quill';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,8 +12,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
 import { PostComponent } from './shared/components/post/post.component';
 import { AuthInterceptor } from './shared/auth.interceptor';
-import { QuillModule } from 'ngx-quill';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
 
 // entire interceptor can be registered also in providers
 const INTERCEPTOR_PROVIDER: Provider = {
@@ -33,7 +35,13 @@ const INTERCEPTOR_PROVIDER: Provider = {
     AppRoutingModule,
     QuillModule.forRoot(),
       FormsModule,
-      ReactiveFormsModule
+      ReactiveFormsModule,
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: environment.production,
+        // Register the ServiceWorker as soon as the app is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000'
+      })
   ],
   // exports: [
   //   HttpClientModule,
